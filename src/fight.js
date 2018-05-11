fd7a91a50f272317259a677e3dece8394dc5c016
didukhn <37545374+didukhn@users.noreply.github.com>
2018-05-11 22:17:44 +0300

    Fixed all errors

--- src/fight.js
+++ src/fight.js
@@ -1,1 +1,32 @@
+import { Fighter } from "./fighter.js";
+import { ImprovedFighter } from "./improvedFighter.js";
+
 // create async function fight
+export async function fight(fighter, improvedFighter, ...points) {
+  let i = 0;
+  while (
+    fighter.health >= 0 &&
+    improvedFighter.health >= 0 &&
+    i < points.length
+  ) {
+    fighter.hit(improvedFighter, points[i]);
+    improvedFighter.doubleHit(fighter, points[i]);
+    ++i;
+  }
+
+  if (fighter.health <= 0) {
+    console.log("user in knockout");
+    let promise = fighter.knockout();
+    await promise.then(result => {
+      console.log("winner is" + improvedFighter.name + "!!!");
+    });
+  } else if (improvedFighter.health <= 0) {
+    console.log("user in knockout");
+    let promise = improvedFighter.knockout();
+    await promise.then(result => {
+      console.log("winner is" + fighter.name + "!!!");
+    });
+  } else {
+    console.log("Both are alive :(");
+  }
+}
--- src/index.js
+++ src/index.js
@@ -1,5 +1,10 @@
 import "babel-polyfill";
+import { Fighter } from "./fighter.js";
+import { ImprovedFighter } from "./improvedFighter.js";
+import { fight } from "./fight.js";
 
 // create two instances
-
+var fighter = new Fighter("Fighter", 100, 2);
+var improvedFighter = new ImprovedFighter("improvedFighter", 100, 3);
 // call fight function
+fight(fighter, improvedFighter, 10, 10, 10);
